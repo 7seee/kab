9575b55faf674969689a8965f02af1fe7a8b6e31
7seee <69136624+7seee@users.noreply.github.com>
2021-09-09 23:56:52 +0900

    modified:   deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ then
   echo "Which folder do you want to deploy to GitHub Pages?"
   exit 1
 fi
-git subtree push --prefix $1 origin gh-pages
+git push origin `git subtree split --prefix=$1 --onto=origin/gh-pages`:gh-pages --force
+
